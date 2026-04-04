0b6d22ebc9d0e6f1faa53df391bcab4a19b78472
Topazzio <patosclive@gmail.com>
2026-04-04 15:22:48 -0300

    eliminate spanish

--- internal/usecase/create_coordinator/usecase.go
+++ internal/usecase/create_coordinator/usecase.go
@@ -19,7 +19,7 @@ func NewUseCase(repo Repository) *UseCase {
 }
 
 func (u *UseCase) Execute(ctx context.Context, user domain.User) (domain.User, error) {
-	user.Type = "coordinator" // 👈 CLAVE
+	user.Type = "coordinator"
 
 	return u.repository.Create(ctx, user)
 }
